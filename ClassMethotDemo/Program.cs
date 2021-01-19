9b4a82c8b81e1028ee09aa91b14a7a9980ac6780
yusuf koç <yusuf_koc72@hotmail.com>
2021-01-19 17:36:47 +0300

    Add project files.

---
+++ ClassMethotDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassMethotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
